30291ec391d7273ff2fdc7cb1907aa000a8905b7
dxminique <do.cofre@duocuc.cl>
2025-10-31 23:22:54 -0300

    creo el screen de navbar y se agregaron algunos colores de fondo a las pantallas

--- app/src/main/java/com/example/p2_apli_huertohogar/MainActivity.kt
+++ app/src/main/java/com/example/p2_apli_huertohogar/MainActivity.kt
@@ -3,52 +3,43 @@ package com.example.p2_apli_huertohogar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.example.p2_apli_huertohogar.repository.InMemoryPedidoRepository
-import com.example.p2_apli_huertohogar.repository.InMemoryProductoRepository
-import com.example.p2_apli_huertohogar.repository.InMemoryUsuarioRepository
-import com.example.p2_apli_huertohogar.ui.screens.HomeScreen
-import com.example.p2_apli_huertohogar.ui.screens.PedidosScreen
-import com.example.p2_apli_huertohogar.ui.screens.ProductoScreen
-import com.example.p2_apli_huertohogar.ui.screens.LoginScreen
-import com.example.p2_apli_huertohogar.ui.screens.RegistroScreen
+import androidx.compose.material3.*
+import androidx.compose.runtime.getValue
+import androidx.navigation.compose.*
+import com.example.p2_apli_huertohogar.ui.screens.*
 import com.example.p2_apli_huertohogar.ui.theme.P2_Apli_HuertoHogarTheme
-import com.example.p2_apli_huertohogar.viewModel.PedidoViewModel
-import com.example.p2_apli_huertohogar.viewModel.ProductoViewModel
-import com.example.p2_apli_huertohogar.viewModel.UsuarioViewModel
-class MainActivity : ComponentActivity() {
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Modifier
 
+
+
+
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             P2_Apli_HuertoHogarTheme {
-                Surface(color = MaterialTheme.colorScheme.background) {
-                    val navController = rememberNavController()
+                val navController = rememberNavController()
+                val currentBackStack by navController.currentBackStackEntryAsState()
+                val currentRoute = currentBackStack?.destination?.route
+
+                Scaffold(
+                    bottomBar = {
+                        if (currentRoute !in listOf("login", "registro")) {
+                            BottomNavBar(navController)
+                        }
+                    }
+                ) { innerPadding ->
                     NavHost(
                         navController = navController,
-                        startDestination = "home"
+                        startDestination = "login",
+                        modifier = Modifier.padding(innerPadding)
                     ) {
-                        composable("home") {
-                            HomeScreen(navController = navController)
-                        }
-                        composable("productos") {
-                            ProductoScreen(navController)
-                        }
-                        composable("pedidos")
-                        { PedidosScreen(navController) }
-
-                        composable("login")
-                        { LoginScreen(navController) }
-
-                        composable("registro")
-                        { RegistroScreen(navController) }
-
-
-
+                        composable("login") { LoginScreen(navController) }
+                        composable("home") { HomeScreen(navController) }
+                        composable("registro") { RegistroScreen(navController) }
+                        composable("productos") { ProductoScreen(navController) }
+                        composable("pedidos") { PedidosScreen(navController) }
                     }
                 }
             }
